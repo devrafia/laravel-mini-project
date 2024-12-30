9b7172803cf4165aa2528b610fb726420e1c64c9
rafia <akmalrafi1132@gmail.com>
2024-12-30 23:15:36 +0700

    fitur search & kategori

--- app/Livewire/Book.php
+++ app/Livewire/Book.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Book as ModelsBook;
+use App\Models\Category;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,11 +12,27 @@ class Book extends Component
     use WithPagination;
 
     public $paginate = 10;
+    public $search;
+    public $category = null;
 
     public function render()
     {
+        $books = ModelsBook::latest()
+            ->when($this->category, function ($query) {
+                return $query->where('category_id', $this->category);
+            })
+            ->when($this->search, function ($query) {
+                return $query->where('title', 'like', '%' . $this->search . '%');
+            })
+            ->paginate($this->paginate);
         return view('livewire.book', [
-            'books' => ModelsBook::latest()->paginate($this->paginate)
+            'books' => $books,
+            'categories' => Category::get(),
         ]);
+    }
+
+    public function updatedSearch()
+    {
+        $this->resetPage();
     }
 }
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -8,37 +8,8 @@
                 Buku</a>
         </div>
         <div class="relative overflow-x-auto sm:rounded-lg">
-
             <livewire:book />
         </div>
     </div>
-    <x-slot name="scripts">
-        <script>
-            document.querySelectorAll('.delete-book').forEach(button => {
-                button.addEventListener('click', function() {
-                    const bookId = this.getAttribute('data-id');
-                    const confirmation = confirm('Apakah Anda yakin ingin menghapus buku ini?');
-                    if (confirmation) {
-                        fetch(`/books/${bookId}`, {
-                                method: 'DELETE',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                                }
-                            })
-                            .then(response => response.json())
-                            .then(data => {
-                                alert(data.message);
-                                // Hapus baris buku dari tabel
-                                this.closest('tr').remove();
-                            })
-                            .catch(error => {
-                                console.error('Error:', error);
-                                alert('Terjadi kesalahan saat menghapus buku.');
-                            });
-                    }
-                });
-            });
-        </script>
-    </x-slot>
+
 </x-layouts.app>
